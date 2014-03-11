3b8d8f76cba15ed814c2d80120e27f17e27a056a
Szabó Gyula <gyufi@niif.hu>
2014-03-11 16:56:29 +0100

    yavom bugs.

--- lib/AttributeResolver/Yavom.php
+++ lib/AttributeResolver/Yavom.php
@@ -2,31 +2,32 @@
 class sspmod_aa_AttributeResolver_Yavom extends sspmod_aa_AttributeResolver
 {
 
-	public function __construct()
-	{
-                parent::__construct();
-		// it is necessery for not come php notice of the opened session.
-		session_destroy();
-		/*  Include YAVOM symfony class. Not part of simpleSAMLphp  */		
-		require_once(dirname(__FILE__).'../../../../../../../../config/ProjectConfiguration.class.php');
-		$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'dev', true);
-		sfContext::createInstance($configuration);
-	}
+        public function __construct($config)
+        {
+                parent::__construct($config);
+                // it is necessery for not come php notice of the opened session.
+                //session_destroy();
+                /*  Include YAVOM symfony class. Not part of simpleSAMLphp  */
+                require_once(dirname(__FILE__).'../../../../../../../../config/ProjectConfiguration.class.php');
+                $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'dev', true);
+                sfContext::createInstance($configuration);
+        }
 
-	public function getAttributes($spid, $eppn, $attributes = array())
-	{
-		/*  AttributeResolver class from YAVOM, not SSP */
-		/*  Getting the related attributes for user and the related SP from YAVOM database */
-		$epe = 'urn:oid:1.3.6.1.4.1.5923.1.1.1.7';
-		$retarray = array();
+        public function getAttributes($eppn, $spid, $attributes = array())
+        {
+                /*  AttributeResolver class from YAVOM, not SSP */
+                /*  Getting the related attributes for user and the related SP from YAVOM database */
+                $epe = 'urn:oid:1.3.6.1.4.1.5923.1.1.1.7';
+                $retarray = array();
 
-		$ar = new AttributeResolver();
-		$uris = $ar->getUrisForEppnToSpid($eppn,$spid);
+                $ar = new AttributeResolver();
+SimpleSAML_Logger::debug('[aa][yavom] Get attributes from resolver, nameid: '.$eppn.' to sp:'. $spid);
+                $uris = $ar->getUrisForEppnToSpid($eppn,$spid);
 
 
-		foreach ($uris as $uri){
-			$retarray[$epe][] = $uri;				
-		}
-		return $retarray;
-	}
+                foreach ($uris as $uri){
+                        $retarray[$epe][] = $uri;
+                }
+                return $retarray;
+        }
 }
