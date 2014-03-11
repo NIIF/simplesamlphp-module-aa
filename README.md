dd1b31258944e01a3870a90bafc1524de2e232d3
Szabó Gyula <gyufi@niif.hu>
2014-03-11 13:39:18 +0100

    rigid authenticity checking

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Attribute Authority for simpleSAMLphp
 
 Derived from (https://code.google.com/p/aa4ssp)
+
+
+The apache2 required config for simplesamlphp location:
+
+        SSLOptions +StdEnvVars +ExportCertData
+        SSLVerifyClient optional_no_ca
--- config-templates/module_aa.php
+++ config-templates/module_aa.php
@@ -10,33 +10,34 @@ $config = array(
 		 * AttributeResolver class
      *
      */
-		//'resolver' => 'Yavom',
-
-		/*
-		 'testvalue' => array(
-		 		'urn:oid:1.3.6.1.4.1.5923.1.1.1.7'=>'test1_Entitlement_from_YAVOM_UsingOnlyTest',
-		 ),
-     */
+		//'resolver' => 'Yavom',		
   
-      'resolver' => 'PDO',
+    /**
+     * PDO class
+     */
 
+      /*
+      'resolver' => 'PDO',
 
       'dsn' => 'mysql:host=localhost;dbname=aadatabase',
       'username' => 'user',
       'password' => 'pass',
 
       'fid_attribute_name'=>'eduPersonPrincipalName',
-
+       */
        /**
         * Must have {{ fid }} tag for given persistent ID
         * like eduPersonPrincipalName
         * select ONLY ONE column. Othervise the first column of result will be processed.
        */
+
+       /*
       'select' => "SELECT urn FROM aa WHERE persistentID = {{ fid }}",
 
       'spid_urnregexp_map' => array(
-              'https://noc2b.vh.hbone.hu/shibboleth' => array('cvsweb'),
-      ),
+              'https://www.example.com/shibboleth' => array('cvsweb'),
+              ),
+      */
 
       /**
        *
@@ -52,7 +53,7 @@ $config = array(
                 ),
         'urn:oid:0.9.2342.19200300.100.1.3' => array(
                 'testuser+from-examplestatic@example.com' //mail
-                )
+                ),
       )
       */
 );
