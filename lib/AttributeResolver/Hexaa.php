56bf5f3438601865a895ed2adb5db8cca0ec5d23
Soltész Balázs <solazs@sztaki.hu>
2014-03-18 12:15:56 +0100

    Hexaa Resolver javítások

---
+++ lib/AttributeResolver/Hexaa.php
@@ -0,0 +1,44 @@
+<?php
+class sspmod_aa_AttributeResolver_Hexaa extends sspmod_aa_AttributeResolver
+{
+
+	public function __construct($config)
+	{
+		private $aa_config;
+		
+                parent::__construct();
+		// it is necessery for not come php notice of the opened session.
+		//session_destroy();
+		$this->aa_config = $config;
+	}
+	
+	private function generateApiKey(){
+		date_default_timezone_set('UTC');
+		$time = new \DateTime();
+		$tstamp = $time->format('Y-m-d H:i');
+		$apiKey = hash('sha256',"simplesamlphp"."ssp".$stamp);
+		return $apiKey;
+	}
+
+	public function getAttributes($spid, $eppn, $attributes = array())
+	{
+		
+		$epe = 'urn:oid:1.3.6.1.4.1.5923.1.1.1.7';
+		$retarray = array();
+		
+		$apiKey = generateApiKey();		
+		$query($aa_config->getValue('hexaa_api_url')."/attributes/".urlencode($eppn)."/".urlencode($spid)."?apikey=".$apikey);
+		
+		$ch = curl_init($query);
+		$options = array(
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_HTTPHEADER => array('Content-type: application/json')		
+		);
+		curl_setopt_array($ch, $options);
+		$result = curl_exec($ch);
+		
+		$data = json_decode($result);
+		return $data;
+	}
+}
+ 
---
+++ templates/settings-hexaa-tpl.php
@@ -0,0 +1,16 @@
+<?php
+
+$this->data['header'] = $this->t('aa_header');
+$this->includeAtTemplateBase('includes/header.php');
+?>
+
+<dl>
+<dt><?php echo $this->t('resolver') ?></dt>
+<dd>Hexaa</dd>
+</dl>
+
+</div>
+
+<?php
+$this->includeAtTemplateBase('includes/footer.php');
+?>
