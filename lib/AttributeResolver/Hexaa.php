f96d2dc617014617f1286394e2cef287db1d84a5
Soltész Balázs <solazs@sztaki.hu>
2014-05-05 12:41:17 +0200

    Hexaa resolver fix.

--- lib/AttributeResolver/Hexaa.php
+++ lib/AttributeResolver/Hexaa.php
@@ -4,42 +4,25 @@ class sspmod_aa_AttributeResolver_Hexaa extends sspmod_aa_AttributeResolver
 
 	public function __construct($config)
 	{
-		private $aa_config;
-		
-                parent::__construct();
-		// it is necessery for not come php notice of the opened session.
-		//session_destroy();
-		$this->aa_config = $config;
-	}
-	
-	private function generateApiKey(){
-		date_default_timezone_set('UTC');
-		$time = new \DateTime();
-		$tstamp = $time->format('Y-m-d H:i');
-		$apiKey = hash('sha256',"simplesamlphp"."ssp".$stamp);
-		return $apiKey;
+				
+                parent::__construct($config);
 	}
 
 
 	public function getAttributes($eppn, $spid, $attributes = array())
 	{
+		$config = $this->config;
 		
 		$epe = 'urn:oid:1.3.6.1.4.1.5923.1.1.1.7';
 		$retarray = array();
 		
-		$apiKey = generateApiKey();	
-		$query($aa_config->getValue('hexaa_api_url')."/attributes/".urlencode($eppn)."?soid=".urlencode($spid)."&apikey=".$apikey);
-
-		
-		$ch = curl_init($query);
-		$options = array(
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_HTTPHEADER => array('Content-type: application/json')		
-		);
-		curl_setopt_array($ch, $options);
-		$result = curl_exec($ch);
-		
-		$data = json_decode($result);
+		date_default_timezone_set('UTC');
+		$time = new \DateTime();
+		$stamp = $time->format('Y-m-d H:i');
+		$apiKey = hash('sha256',"simplesamlphp"."ssp".$stamp);	
+		$query = $config->getValue('hexaa_api_url')."/attributes/".urlencode($eppn)."?soid=".urlencode($spid)."&apikey=".$apiKey;
+		$result = file_get_contents($query);
+		$data = json_decode($result, true);
 		return $data;
 	}
 }
