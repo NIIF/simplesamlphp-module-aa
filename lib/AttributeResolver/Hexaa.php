81e5b7985ce3972598c08f4b5ad1cbf185843d98
Soltész Balázs <solazs@sztaki.hu>
2014-06-04 12:10:58 +0200

    Hexaa resolver rendes fix + debug logging

--- lib/AttributeResolver/Hexaa.php
+++ lib/AttributeResolver/Hexaa.php
@@ -15,12 +15,12 @@ class sspmod_aa_AttributeResolver_Hexaa extends sspmod_aa_AttributeResolver
 		
 		$epe = 'urn:oid:1.3.6.1.4.1.5923.1.1.1.7';
 		$retarray = array();
-		
-		date_default_timezone_set('UTC');
+	        date_default_timezone_set('UTC');	
 		$time = new \DateTime();
 		$stamp = $time->format('Y-m-d H:i');
 		$apiKey = hash('sha256',"simplesamlphp"."ssp".$stamp);	
-		$query($aa_config->getValue('hexaa_api_url')."/attributes?fedid=".urlencode($eppn)."&soid=".urlencode($spid)."&apikey=".$apikey);
+		SimpleSAML_Logger::debug('[aa] Hexaa api query: '.$config->getValue('hexaa_api_url')."/attributes.json?fedid=".urlencode($eppn)."&soid=".urlencode($spid)."apikey=HIDDEN with timestamp: ".$stamp);
+		$query = $config->getValue('hexaa_api_url')."/attributes.json?fedid=".urlencode($eppn)."&soid=".urlencode($spid)."&apikey=".$apiKey;
 		$result = file_get_contents($query);
 		$data = json_decode($result, true);
 		return $data;
