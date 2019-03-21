17b448e39414afb6f2a642ede77c534732c59a93
Szabó Gyula <szabo.gyula@sztaki.mta.hu>
2019-03-21 21:27:52 +0100

    namespaced classes

--- lib/AA/SAML2.php
+++ lib/AA/SAML2.php
@@ -22,7 +22,7 @@ class sspmod_aa_AA_SAML2
 
     public function __construct($metadata)
     {
-        $this->config = SimpleSAML_Configuration::getConfig('module_aa.php');
+        $this->config = SimpleSAML\Configuration::getConfig('module_aa.php');
 
         $this->signAssertion = false;
         if ($this->config->hasValue('signAssertion')) {
@@ -43,12 +43,12 @@ class sspmod_aa_AA_SAML2
     public function getBinding()
     {
         /* Receiving the attribute query */
-        $binding = SAML2_Binding::getCurrentBinding();
-        SimpleSAML_Logger::debug('[aa] binding: '.var_export($binding, true));
+        $binding = SAML2\Binding::getCurrentBinding();
+        SimpleSAML\Logger::debug('[aa] binding: '.var_export($binding, true));
 
         /* Supported binding is SOAP */
-        if (!($binding instanceof SAML2_SOAP || $binding instanceof SAML2\SOAP)) {
-            throw new SimpleSAML_Error_BadRequest('[aa] Unsupported binding. It must be SAML2_SOAP.');
+        if (!($binding instanceof SAML2\SOAP)) {
+            throw new SimpleSAML\Error\BadRequest('[aa] Unsupported binding. It must be SAML2\SOAP.');
         }
 
         return $binding;
@@ -57,10 +57,10 @@ class sspmod_aa_AA_SAML2
     private function getQuery()
     {
         $query = $this->binding->receive();
-        SimpleSAML_Logger::debug('[aa] query: '.var_export($query, true));
+        SimpleSAML\Logger::debug('[aa] query: '.var_export($query, true));
 
-        if (!($query instanceof SAML2_AttributeQuery || $query instanceof SAML2\AttributeQuery)) {
-            throw new SimpleSAML_Error_BadRequest('Invalid message received on AttributeQuery endpoint.');
+        if (!($query instanceof SAML2\AttributeQuery)) {
+            throw new SimpleSAML\Error\BadRequest('Invalid message received on AttributeQuery endpoint.');
         }
 
         return $query;
@@ -74,7 +74,7 @@ class sspmod_aa_AA_SAML2
 
         $spEntityId = $this->query->getIssuer();
         if ($spEntityId === null) {
-            throw new SimpleSAML_Error_BadRequest('Missing <saml:Issuer> in <samlp:AttributeQuery>.');
+            throw new SimpleSAML\Error\BadRequest('Missing <saml:Issuer> in <samlp:AttributeQuery>.');
         }
         $spMetadata = $metadata->getMetaDataConfig($spEntityId, 'saml20-sp-remote');
 
@@ -87,8 +87,7 @@ class sspmod_aa_AA_SAML2
     private function getAttributeNameFormat()
     {
         /* The name format of the attributes. */
-        //$attributeNameFormat = SAML2_Const::NAMEFORMAT_URI;
-        $attributeNameFormat = 'urn:oasis:names:tc:SAML:2.0:attrname-format:uri';
+        $attributeNameFormat = SAML2\Constants::NAMEFORMAT_URI;
         if ($this->config->hasValue('attributeNameFormat')) {
             $attributeNameFormat = $this->config->getValue('attributeNameFormat');
         }
@@ -123,35 +122,35 @@ class sspmod_aa_AA_SAML2
 
         /* Authenticate the requestor by verifying the TLS certificate used for the HTTP query */
         if (array_key_exists('SSL_CLIENT_VERIFY', $_SERVER)) {
-            SimpleSAML_Logger::debug('[aa] Request was made using the following certificate: '.var_export($_SERVER['SSL_CLIENT_VERIFY'], 1));
+            SimpleSAML\Logger::debug('[aa] Request was made using the following certificate: '.var_export($_SERVER['SSL_CLIENT_VERIFY'], 1));
         }
         if (array_key_exists('SSL_CLIENT_VERIFY', $_SERVER) && $_SERVER['SSL_CLIENT_VERIFY'] && $_SERVER['SSL_CLIENT_VERIFY'] != 'NONE') {
             /* compare certificate fingerprints */
             $clientCertData = trim(preg_replace('/--.* CERTIFICATE-+-/', '', $_SERVER['SSL_CLIENT_CERT']));
             $clientCertFingerprint = strtolower(sha1(base64_decode($clientCertData)));
             if (!$clientCertFingerprint) {
-                throw new SimpleSAML_Error_Exception('[aa] Can not calculate certificate fingerprint from the request.');
+                throw new SimpleSAML\Error\Exception('[aa] Can not calculate certificate fingerprint from the request.');
             }
 
-            $spCertArray = SimpleSAML_Utilities::loadPublicKey($this->spMetadata);
+            $spCertArray = SimpleSAML\Utils\Crypto::loadPublicKey($this->spMetadata);
             if (!$spCertArray) {
-                throw new SimpleSAML_Error_Exception('[aa] Can not find the public key of the requestor in the metadata!');
+                throw new SimpleSAML\Error\Exception('[aa] Can not find the public key of the requestor in the metadata!');
             }
 
             foreach ($spCertArray['certFingerprint'] as $fingerprint) {
                 if ($fingerprint && $clientCertFingerprint == $fingerprint) {
                     $client_is_authenticated = true;
-                    SimpleSAML_Logger::debug('[aa] SSL certificate is checked and valid.');
+                    SimpleSAML\Logger::debug('[aa] SSL certificate is checked and valid.');
                     break;
                 }
             }
             /* Reject the request if the TLS certificate used for the request does not match metadata */
             if (!$client_is_authenticated) {
-                throw new SimpleSAML_Error_Exception('[aa] SSL certificate check failed.');
+                throw new SimpleSAML\Error\Exception('[aa] SSL certificate check failed.');
             }
         } else {
             /* The request may be signed, so this is not fatal */
-            SimpleSAML_Logger::debug('[aa] SSL client certificate does not exist.');
+            SimpleSAML\Logger::debug('[aa] SSL client certificate does not exist.');
         }
 
         /* Authenticate the requestor by verifying the XML signature on the query */
@@ -159,24 +158,24 @@ class sspmod_aa_AA_SAML2
         if (count($certs_of_query) > 0) {
             if (sspmod_saml_Message::checkSign($this->spMetadata, $this->query)) {
                 $client_is_authenticated = true;
-                SimpleSAML_Logger::debug('[aa] AttributeQuery signature is checked and valid.');
+                SimpleSAML\Logger::debug('[aa] AttributeQuery signature is checked and valid.');
             } else {
                 /* An invalid or unverifiable signature is fatal */
-                throw new SimpleSAML_Error_Exception('[aa] The signature of the AttributeQuery is wrong!');
+                throw new SimpleSAML\Error\Exception('[aa] The signature of the AttributeQuery is wrong!');
             }
         } else {
             /* The request may be protected by HTTP TLS (X.509) authentication, so this is not fatal */
-            SimpleSAML_Logger::debug('[aa] AttributeQuery has no signature.');
+            SimpleSAML\Logger::debug('[aa] AttributeQuery has no signature.');
         }
 
         if (!$client_is_authenticated) {
-            SimpleSAML_Logger::info('[aa] Attribute query was not authenticated. Drop.');
+            SimpleSAML\Logger::info('[aa] Attribute query was not authenticated. Drop.');
             header('HTTP/1.1 401 Unauthorized');
             header('WWW-Authenticate: None', false);
             echo 'Not authenticated. Neither query signature nor SSL client certificate was available.';
             exit;
         } else {
-            SimpleSAML_Logger::debug('[aa] Attribute query was authenticated.');
+            SimpleSAML\Logger::debug('[aa] Attribute query was authenticated.');
         }
     }
 
@@ -185,13 +184,14 @@ class sspmod_aa_AA_SAML2
         $nameId = $this->query->getNameId();
 
         if (!$nameId) {
-            throw new SimpleSAML_Error_BadRequest('[aa] Error getting NameID from AttributeQuery.');
+            throw new SimpleSAML\Error\BadRequest('[aa] Error getting NameID from AttributeQuery.');
         }
-        if (array_key_exists('Format', $nameId)) {
-            $nameIdFormat = $nameId['Format'];
+        if ($nameId->getFormat()) {
+            $nameIdFormat = $nameId->getFormat();
         }
+        $nameIdValue = $nameId->getValue();
 
-        SimpleSAML_Logger::info('[aa] Received attribute query for '.$nameId['Value'].' (nameIdFormat: '.$nameIdFormat.')');
+        SimpleSAML\Logger::info('[aa] Received attribute query for '.$nameIdValue.' (nameIdFormat: '.$nameIdFormat.')');
 
         /* Get the attributes from the AuthSource */
         $spMetadataArray = $this->spMetadata->toArray();
@@ -201,11 +201,11 @@ class sspmod_aa_AA_SAML2
             'Attributes' => $attributes,
             'Destination' => $spMetadataArray,
             'Source' => $aaMetadataArray,
-            'aa:nameId' => $nameId['Value'],
+            'aa:nameId' => $nameIdValue,
             'aa:nameIdFormat' => $nameIdFormat,
         );
 
-        $as = SimpleSAML_Auth_Source::getById($this->config->getValue('authsource'));
+        $as = SimpleSAML\Auth\Source::getById($this->config->getValue('authsource'));
         $as->authenticate($state);
 
         $attributes = $state['Attributes'];
@@ -217,7 +217,7 @@ class sspmod_aa_AA_SAML2
     {
         $spMetadataArray = $this->spMetadata->toArray();
         $aaMetadataArray = $this->aaMetadata->toArray();
-        $pc = new SimpleSAML_Auth_ProcessingChain($aaMetadataArray, $spMetadataArray, 'aa');
+        $pc = new SimpleSAML\Auth\ProcessingChain($aaMetadataArray, $spMetadataArray, 'aa');
         $authProcState = array(
             'Attributes' => $attributes,
             'Destination' => $spMetadataArray,
@@ -231,11 +231,11 @@ class sspmod_aa_AA_SAML2
     {
         $requestedAttributes = $this->query->getAttributes();
         if (count($requestedAttributes) === 0) {
-            SimpleSAML_Logger::debug(
+            SimpleSAML\Logger::debug(
                 '[aa] No attributes requested - return all previously resolved attributes: '.var_export($attributes, true)
             );
         } elseif ($this->query->getAttributeNameFormat() !== $this->attributeNameFormat) {
-            SimpleSAML_Logger::debug(
+            SimpleSAML\Logger::debug(
                 '[aa] NameFormat mismatch - no attributes returned. Expected: '.$this->attributeNameFormat.' Requested: '.$this->query->getAttributeNameFormat()
             );
             $attributes = array();
@@ -243,7 +243,7 @@ class sspmod_aa_AA_SAML2
             foreach ($attributes as $name => $values) {
                 if (!array_key_exists($name, $requestedAttributes)) {
                     /* They didn't request this attribute. */
-                    SimpleSAML_Logger::debug('[aa] Remove attribute because it was not requested: '.$name);
+                    SimpleSAML\Logger::debug('[aa] Remove attribute because it was not requested: '.$name);
                     unset($attributes[$name]);
                     continue;
                 }
@@ -262,14 +262,14 @@ class sspmod_aa_AA_SAML2
     private function buildResponse($returnAttributes)
     {
         /* SubjectConfirmation */
-        $sc = new SAML2_XML_saml_SubjectConfirmation();
-        $sc->Method = SAML2_Const::CM_BEARER;
-        $sc->SubjectConfirmationData = new SAML2_XML_saml_SubjectConfirmationData();
+        $sc = new SAML2\XML\saml\SubjectConfirmation();
+        $sc->Method = SAML2\Constants::CM_BEARER;
+        $sc->SubjectConfirmationData = new SAML2\XML\saml\SubjectConfirmationData();
         $sc->SubjectConfirmationData->NotBefore = time();
         $sc->SubjectConfirmationData->NotOnOrAfter = time() + $this->config->getInteger('validFor');
         $sc->SubjectConfirmationData->InResponseTo = $this->query->getId();
 
-        $assertion = new SAML2_Assertion();
+        $assertion = new SAML2\Assertion();
         $assertion->setSubjectConfirmation(array($sc));
         $assertion->setIssuer($this->aaEntityId);
         $assertion->setNameId($this->query->getNameId());
@@ -283,7 +283,7 @@ class sspmod_aa_AA_SAML2
         }
 
         /* The Response */
-        $response = new SAML2_Response();
+        $response = new SAML2\Response();
         $response->setRelayState($this->query->getRelayState());
         $response->setIssuer($this->aaEntityId);
         $response->setInResponseTo($this->query->getId());
@@ -297,8 +297,8 @@ class sspmod_aa_AA_SAML2
 
     private function sendResponse($response)
     {
-        SimpleSAML_Logger::debug('[aa] Sending: '.var_export($response, true));
-        SimpleSAML_Logger::info('[aa] Sending assertion.');
+        SimpleSAML\Logger::debug('[aa] Sending: '.var_export($response, true));
+        SimpleSAML\Logger::info('[aa] Sending assertion.');
         $this->binding->send($response);
     }
 }
